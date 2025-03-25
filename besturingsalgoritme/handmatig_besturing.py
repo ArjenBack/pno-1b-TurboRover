9d2bc861f2b8d3cc07e9c44a175ae053246cdfaf
ArjenBack <arjen@backeljauw.be>
2025-03-25 15:08:59 +0100

    Improve line following and turning logic for robot
    The changes enhance the robot's navigation by adjusting motor speeds, refining
    turn detection, and adding debug output. Includes a continuous drive-turn loop
    and better black line detection during turns.

--- besturingsalgoritme/handmatig_besturing.py
+++ besturingsalgoritme/handmatig_besturing.py
@@ -3,7 +3,6 @@ import time
 import pwmio
 import digitalio
 from analogio import AnalogIn
-from tkinter.constants import ARC
 
 ### Defineren van de pinnen
 
@@ -59,7 +58,7 @@ def drive_line():
     LDR_rechts_value = LDR_rechts.value
     LDR_achter_value = LDR_achter.value
 
-    drive_forward(1)
+    drive_forward(0.5)
 
     while not crossroad_found:
         time.sleep(0.1)
@@ -72,6 +71,7 @@ def drive_line():
         LDR_rechts_value = LDR_rechts.value
         LDR_achter_value = LDR_achter.value
 
+        print("ACHTER: prev %s, current: %s, diff: %s" % (prev_LDR_achter_value, LDR_achter_value, abs(prev_LDR_achter_value - LDR_achter_value)))
         # aanpassing links
         if LDR_links_value - LDR_rechts_value < -18000:
             motor_rechts.duty_cycle = 15000
@@ -82,15 +82,17 @@ def drive_line():
             motor_links.duty_cycle = 15000
             motor_rechts.duty_cycle = 30000
 
+        # Zet motoren gelijk
+        else:
+            drive_forward(0.5)
+
         # kruispunt stop
-        elif abs(prev_LDR_achter_value - LDR_achter_value) > MINIMUM_AFWIJKWAARDE_ACHTER:
+        if abs(prev_LDR_achter_value - LDR_achter_value) > MINIMUM_AFWIJKWAARDE_ACHTER:
+            print("Klaar om te draaien, waar zijn die handjes")
             crossroad_found = True
             motor_links.duty_cycle = 0
             motor_rechts.duty_cycle = 0
 
-        # Zet motoren gelijk
-        else:
-            drive_forward(0.5)
 
 
 def turn_left():
@@ -104,23 +106,33 @@ def turn_left():
     LDR_rechts_value = LDR_rechts.value
     LDR_achter_value = LDR_achter.value
 
+    ref = time.monotonic()
+    black_found = False
     while True:
-        time.sleep(0.1)
+
+        time.sleep(0.05)
         prev_LDR_achter_value = LDR_achter_value
+        prev_LDR_links_value = LDR_links_value
+        prev_LDR_rechts_value = LDR_rechts_value
+
         LDR_links_value = LDR_links.value
         LDR_rechts_value = LDR_rechts.value
         LDR_achter_value = LDR_achter.value
-        print("ACHTER prev: %s, current: %s, diff: %s" % (
-        prev_LDR_achter_value, LDR_achter_value, prev_LDR_achter_value - LDR_achter_value))
+
         print(
             "links: %s, rechts: %s, diff: %s" % (LDR_links_value, LDR_rechts_value, LDR_links_value - LDR_rechts_value))
 
-        if abs(prev_LDR_achter_value - LDR_achter_value) > MINIMUM_AFWIJKWAARDE_ACHTER:
+        if black_found and LDR_links_value - LDR_rechts_value < -16000 and time.monotonic() > (ref + 0.5):
             motor_links.duty_cycle = 0
             motor_rechts.duty_cycle = 0
+            print("stopped")
             break
 
+        if LDR_links_value - LDR_rechts_value < -18000:
+            black_found = True
+            print("Found black")
 
-drive_line()
 
-turn_left()
+while True:
+    drive_line()
+    turn_left()
